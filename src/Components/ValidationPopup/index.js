b9aa38386dd85997af68dd46f8f6176c81b599ea
gauravjainn <46779914+gauravjainn@users.noreply.github.com>
2021-08-26 16:15:35 +0530

    Accept Term screen

---
+++ src/Components/ValidationPopup/index.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react';
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import TransparentButton from '../../Components/TransparentButton';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+const App = ({onPress, Show,showback}) => {
+    // console.log(Show + )
+  const [modalVisible, setModalVisible] = useState(Show);
+  const updateModalstate = () => {
+    setModalVisible(!modalVisible);
+    showback(!modalVisible);
+  }
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View
+          style={{
+            justifyContent: 'flex-start',
+            justifyContent: 'flex-end',
+
+            backgroundColor: '#262A34',
+            borderTopEndRadius: 25,
+            borderTopLeftRadius: 25,
+            marginTop: hp('82%'),
+            paddingBottom:hp('3%')
+          }}>
+          <View style={{marginTop: hp('2%')}}>
+            <Text
+              style={{
+                color: '#5E6272',
+                fontSize: 14,
+                textAlign: 'center',
+                // marginTop: hp('3%'),
+                // backgroundColor:'yellow'
+              }}>
+              You can choose 1 option
+            </Text>
+          </View>
+          <View
+            style={{
+              marginTop: hp('3%'),
+              alignItems: 'center',
+            }}>
+            <TransparentButton
+              title="Got it"
+              onPress={ updateModalstate}
+              style={{
+                width: wp('85%'),
+                height: hp('7.3%'),
+                backgroundColor: '#246BFD',
+                borderColor: '#246BFD',
+                // marginLeft:wp('5%')
+              }}
+              textStyle={{fontWeight: 'bold', fontSize: 14}}
+            />
+          </View>
+        </View>
+      </Modal>
+     
+    </View>
+  );
+};
+
+export default App;
